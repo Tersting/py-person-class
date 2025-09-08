621e57e6ee13df93a7bbedb177e25d76055baac8
Nazar <your@mail.com>
2025-09-08 13:43:38 +0300

    Solution_2

--- app/main.py
+++ app/main.py
@@ -7,23 +7,19 @@ class Person:
         Person.people[self.name] = self
 
 
-def create_person_list(people: list) -> list:
-    for dict_index in people:
-        name = dict_index["name"]
-        age = dict_index["age"]
-        Person(name, age)
+def create_person_list(people: list[dict]) -> list[Person]:
+    [Person(element["name"], element["age"]) for element in people]
 
     for dict_index in people:
         name = dict_index["name"]
-        persone = Person.people[name]
+        person = Person.people[name]
 
-        if dict_index.get("wife") is not None:
-            wife_name = dict_index["wife"]
-            if wife_name in Person.people:
-                persone.wife = Person.people[wife_name]
+        wife_name = dict_index.get("wife")
+        if wife_name and wife_name in Person.people:
+            person.wife = Person.people[wife_name]
 
-        if dict_index.get("husband") is not None:
-            husband_name = dict_index["husband"]
-            if husband_name in Person.people:
-                persone.husband = Person.people[husband_name]
+        husband_name = dict_index.get("husband")
+        if husband_name and husband_name in Person.people:
+            person.husband = Person.people[husband_name]
+
     return [Person.people[name["name"]] for name in people]
